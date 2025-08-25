c434c80fd9ed18e9dfbe43c6e1574c118d82275e
Paolo Prem <prempaolo@gmail.com>
2025-08-25 12:27:09 +0200

    Update airline.lua

--- .config/nvim/lua/plugins/airline.lua
+++ .config/nvim/lua/plugins/airline.lua
@@ -1,8 +1,10 @@
 return {
-   "bling/vim-airline",
-  lazy = false,
-  init = function()
-    vim.cmd("let g:airline_theme='bubblegum'")
-    vim.cmd("let g:airline#extensions#whitespace#symbol = '!'")
-  end,
+  {
+    "bling/vim-airline",
+    lazy = false,
+    init = function()
+      vim.cmd("let g:airline_theme='bubblegum'")
+      vim.cmd("let g:airline#extensions#whitespace#symbol = '!'")
+    end,
+  }
 }
