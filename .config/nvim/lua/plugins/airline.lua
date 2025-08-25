8b4d9bb00aef91313065bfb87fe6c14bad983cab
Paolo Prem <prempaolo@gmail.com>
2025-08-25 12:21:35 +0200

    Transition to lazy nvim

---
+++ .config/nvim/lua/plugins/airline.lua
@@ -0,0 +1,8 @@
+return {
+   "bling/vim-airline",
+  lazy = false,
+  init = function()
+    vim.cmd("let g:airline_theme='bubblegum'")
+    vim.cmd("let g:airline#extensions#whitespace#symbol = '!'")
+  end,
+}
